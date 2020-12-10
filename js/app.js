a11cdfc5f70b1ea765b2ff8b1de4e03e8c625fe1
Prince Codes 247 <princecodes247@gmail.com>
2020-12-10 02:05:49 -0800

    feat: update styles, add new sections

--- js/app.js
+++ js/app.js
@@ -1,23 +1,28 @@
 console.log(`Hi ${navigator.userAgent.toLowerCase()}`);
 const body = document.querySelector("body");
-const grid = document.querySelector('.grid')
+const skillsGrid = document.querySelector('#skills .grid')
+const worksGrid = document.querySelector('#works .grid')
+
 const header = document.querySelector('header')
 const menuBtn = document.querySelector("#ham")
-const stack = "HTML, CSS, Sass, BootStrap, Tailwind, JS, React, Python, Node.JS, MongoDB, Express, Nginx, Linux, Illustrator, Adobe XD, Photoshop, GitHub, Organization, Collaboration, Design, Google, StackOverflow"
-//const stack = "HTML, CSS, Sass, JS, BootStrap, React, PHP, Python, Node.JS, MongoDB, MySQL, Express, Java, C, Nginx, Linux, REST, Apache, Illustrator, Adobe XD, Photoshop, GitHub, Organization, Collaboration, Design, Google, StackOverflow"
+const skills = "HTML/CSS, Sass, BootStrap, Wordpress, Tailwind, JS, React/Redux, Python, Node.JS, Ngrok, Linux, GitHub, Organization, Collaboration, Creative Direction, Google, StackOverflow"
+const works = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11"
+//const stack = "HTML, CSS, Sass, JS, BootStrap, Wordpress, React, PHP, Python, Node.JS, MongoDB, MySQL, Express, Java, C, Nginx, Linux, REST, Apache, Illustrator, Adobe XD, Photoshop, GitHub, Organization, Collaboration, Design, Google, StackOverflow"
 const navMenu = document.querySelector('#nav-menu')
-let stackList = stack.split(", ")
+let skillsList = skills.split(", ")
+let worksList = works.split(", ")
 let menuIsOpen = false
-function addStackToGrid(skill) {
+function addToGrid(item, target) {
     let container = document.createElement('span')
     container.classList.add("grid-item")
     container.setAttribute("data-scroll", '');
     container.setAttribute("data-scroll-class", 'expand');
     let name = document.createElement('span')
-    name.innerText = skill
+    name.innerText = item
     container.appendChild(name)
-    grid.appendChild(container)
+    target.appendChild(container)
 }
 
-stackList.forEach(stack => addStackToGrid(stack))
+skillsList.forEach(skill => addToGrid(skill, skillsGrid))
+worksList.forEach(work => addToGrid(work, worksGrid))
 
