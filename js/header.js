30a2b17d5017a8b25b781659e393a1bd0c3ba926
Prince Codes 247 <princecodes247@gmail.com>
2021-01-04 03:01:57 -0800

    feat: implement switch in waver.js

--- js/header.js
+++ js/header.js
@@ -9,13 +9,14 @@ headers.forEach(header => {
 
 })
 
-function waver(item, target, max, interval, speed, mode = 1) {
+function waver(item, target, max, interval, speed, mode = 3) {
     switch (mode) {
         case 1:
+            let textCenter = Math.floor(item.length / 2)
             for (i = 0; i < item.length; i++) {
                 let a = document.createElement('h2')
-                let b = (i * interval).toFixed(2)
-                let delay = (max - b).toFixed(2).toString()
+                let b = Math.abs((textCenter - i) * interval).toFixed(2)
+                let delay = Math.abs(max - b).toFixed(2).toString()
                 a.setAttribute('data-scroll', '')
                 a.setAttribute('data-scroll-speed', speed)
                 a.setAttribute('data-scroll-delay', delay)
@@ -23,35 +24,34 @@ function waver(item, target, max, interval, speed, mode = 1) {
                 target.appendChild(a)
             }
             break;
-
+        case 2:
+            for (i = 0; i < item.length; i++) {
+                let a = document.createElement('h2')
+                let b = Math.abs(i * interval).toFixed(2)
+                let delay = Math.abs(max - b).toFixed(2).toString()
+                a.setAttribute('data-scroll', '')
+                a.setAttribute('data-scroll-speed', speed)
+                a.setAttribute('data-scroll-delay', delay)
+                a.innerText = item[i]
+                target.appendChild(a)
+            }
+            break;
+        case 3:
+            for (i = 0; i < item.length; i++) {
+                let a = document.createElement('h2')
+                let b = ((item.length - i) * interval).toFixed(2)
+                let delay = Math.abs(max - b).toFixed(2).toString()
+                a.setAttribute('data-scroll', '')
+                a.setAttribute('data-scroll-speed', speed)
+                a.setAttribute('data-scroll-delay', delay)
+                a.innerText = item[i]
+                target.appendChild(a)
+            }
+            break;
         default:
             break;
     }
 }
 
-function centerWave(item, target, max, interval, speed) {
-    let textCenter = Math.floor(item.length / 2)
-    for (i = 0; i < item.length; i++) {
-        let a = document.createElement('h2')
-        let b = Math.abs((textCenter - i) * interval).toFixed(2)
-        let delay = (max - b).toFixed(2).toString()
-        a.setAttribute('data-scroll', '')
-        a.setAttribute('data-scroll-speed', speed)
-        a.setAttribute('data-scroll-delay', delay)
-        a.innerText = item[i]
-        target.appendChild(a)
-    }
-}
 
-function endWave(item, target, max, interval, speed) {
-    for (i = 0; i < item.length; i++) {
-        let a = document.createElement('h2')
-        let b = (i * (item.length - interval)).toFixed(2)
-        let delay = (max - b).toFixed(2).toString()
-        a.setAttribute('data-scroll', '')
-        a.setAttribute('data-scroll-speed', speed)
-        a.setAttribute('data-scroll-delay', delay)
-        a.innerText = item[i]
-        target.appendChild(a)
-    }
-}
+
